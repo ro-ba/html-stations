6f4606995cb153545b3e1e0a2369b5a7dec10eb8
ro-ba <5171028@st.hsc.ac.jp>
2022-09-02 00:30:27 +0900

    railwayクリア

---
+++ src/scripts/station15.js
@@ -0,0 +1,36 @@
+async function getData() {
+  // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
+  // ３秒後にフルネームのリストを表示されるようにしましょう。
+  // 最初から表示されていると、クリアになりません。
+  const userList = [
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affilication: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affilication: "HogeHoge大学",
+      is_student: true,
+    },
+  ];
+
+  const result = await test(userList);
+  await _sleep(3000);
+  return await result;
+}
+
+function test(dataList) {
+  return dataList.map(buildFullName);
+}
+
+function buildFullName(data) {
+  data["full_name"] = `${data["family_name"]} ${data["first_name"]}`;
+  return data;
+}
+
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
